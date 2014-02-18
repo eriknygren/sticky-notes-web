d27062e8c6a009c8cef118ed06469374c5a5b22c
Erik Nygren <erik.nygren6@gmail.com>
2014-02-18 12:53:01 +0000

    Can now refresh /notes -  slight tweak to the server

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 var connect = require('connect');
-connect.createServer(
-    connect.static(__dirname+ '/web-client/')
-).listen(8080);
+var express = require('express');
+var http = require('http');
+var app = express();
+app.use(app.router);
+
+app.use(express.static(__dirname+ '/web-client/'));
+
+http.createServer(app).listen(8080), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+};
+
+app.get('/notes', function(req, res)
+	{
+		res.sendfile(__dirname+ '/web-client/index.html');
+	});
